2727b84d383eeed277a1ef2f28dbc1b35b7d9d24
Edgar Reyes <gzzenit@gmail.com>
2023-11-25 11:31:33 -0600

    room is added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,11 @@ dependencies {
     //livedata
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
 
+    //Room
+    implementation("androidx.room:room-runtime:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
+    kapt("androidx.room:room-compiler:2.6.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
