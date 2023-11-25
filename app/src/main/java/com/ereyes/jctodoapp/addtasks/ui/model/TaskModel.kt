53010244f689981c9dad402ec2fa63ac0b1e3c9d
Edgar Reyes <gzzenit@gmail.com>
2023-11-25 11:33:41 -0600

    update task model

--- app/src/main/java/com/ereyes/jctodoapp/addtasks/ui/model/TaskModel.kt
+++ app/src/main/java/com/ereyes/jctodoapp/addtasks/ui/model/TaskModel.kt
@@ -1,9 +1,17 @@
 package com.ereyes.jctodoapp.addtasks.ui.model
 
+import com.ereyes.jctodoapp.addtasks.data.database.entities.TaskEntity
+
 /****
  * Project: JCTodoApp
  * From: com.ereyes.jctodoapp.addtasks.ui.model
  * Created by Edgar Reyes Gonzalez on 11/24/2023 at 9:43 AM
  * All rights reserved 2023.
  ****/
-data class TaskModel(val id: Long = System.currentTimeMillis(), val task: String, val selected: Boolean = false)
+data class TaskModel(
+    val id: Int = System.currentTimeMillis().hashCode(),
+    val task: String,
+    var selected: Boolean = false
+)
+
+fun TaskEntity.toDomain(): TaskModel = TaskModel(id, task, selected)
